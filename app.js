a5ae0d48d631c83a403d656650beec95eb06de67
Anupama_Halder <anupamahalder2022@gmail.com>
2024-06-10 21:17:47 +0530

    added auto scroll when meal card is clicked

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const handleSearch = ()=>{
     document.getElementById("search-input").value = "";
     // Clear meal-list-container's inner HTML
     document.getElementById("meal-list-container").innerHTML = "";
+    document.getElementById("meal-detail-container").innerHTML = "";
 
     // fetch all matched data 
     fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchValue}`)
@@ -28,42 +29,39 @@ const handleSearch = ()=>{
                 <p><b>${meal.strMeal.slice(0,40)}</b></p>
                 `
                 // Attach event listener to call handleMealDetail function
-                div.addEventListener('click', () => handleMealDetail(meal.idMeal));
+                div.addEventListener('click', () => handleMealDetail(meal));
                 mealListContainer.appendChild(div);
             })
         }
     })
 }
-const handleMealDetail = (id) =>{
+const handleMealDetail = (meal) =>{
     const mealDetail = document.getElementById("meal-detail-container");
     // clear the div 
     document.getElementById("meal-detail-container").innerHTML = "";
-    fetch(`https:\\www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`)
-    .then(res => res.json())
-    .then(meal => {
-        meal = meal.meals[0];
-        console.log(meal);
-        const ingredients = [];
-        for (let i = 1; i <= 20; i++) {
-            const ingredient = meal[`strIngredient${i}`];
-            const measure = meal[`strMeasure${i}`];
-            if (ingredient && measure) {
-            ingredients.push(`${measure.trim()} ${ingredient}`);
-            } else if (ingredient) {
-            ingredients.push(ingredient);
-            }
+    console.log(meal);
+    const ingredients = [];
+    for (let i = 1; i <= 20; i++) {
+        const ingredient = meal[`strIngredient${i}`];
+        const measure = meal[`strMeasure${i}`];
+        if (ingredient && measure) {
+        ingredients.push(`${measure.trim()} ${ingredient}`);
+        } else if (ingredient) {
+        ingredients.push(ingredient);
         }
-        // create a div 
-        const div = document.createElement("div");
-        div.classList = "meal-detail-card";
-        div.innerHTML = `
-        <img class="meal-detail-img" src="${meal.strMealThumb}"/>
-        <h3>${meal.strMeal}</h3>
-        <h5>Ingredients</h5>
-        <ul>
-            ${ingredients.map(ingredient => `<li>${ingredient}</li>`).join('')}
-        </ul>
-        `
-        mealDetail.appendChild(div);
-    })
+    }
+    // create a div 
+    const div = document.createElement("div");
+    div.classList = "meal-detail-card";
+    div.innerHTML = `
+    <img class="meal-detail-img" src="${meal.strMealThumb}"/>
+    <h3>${meal.strMeal}</h3>
+    <h5>Ingredients</h5>
+    <ul>
+        ${ingredients.map(ingredient => `<li>${ingredient}</li>`).join('')}
+    </ul>
+    `
+    mealDetail.appendChild(div);
+    // Scroll to the meal detail container
+    mealDetail.scrollIntoView({ behavior: 'smooth', block: 'start' });
 }
